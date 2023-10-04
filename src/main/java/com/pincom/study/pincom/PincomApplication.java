dde116a4b34b838b96436d51e64ab0ec6965c236
jayong <jyong6652@naver.com>
2023-10-04 16:04:11 +0900

    start

---
+++ src/main/java/com/pincom/study/pincom/PincomApplication.java
@@ -0,0 +1,13 @@
+package com.pincom.study.pincom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PincomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PincomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pincom/study/pincom/PincomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pincom.study.pincom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PincomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
